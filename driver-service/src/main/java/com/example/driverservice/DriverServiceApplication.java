f1cb094c3fb2db18bd98a1b0b2cb7160389f4919
Jaleel Adesina <jtutoff@gmail.com>
2020-09-13 08:13:45 +0100

    added circuitbreaker

--- driver-service/src/main/java/com/example/driverservice/DriverServiceApplication.java
+++ driver-service/src/main/java/com/example/driverservice/DriverServiceApplication.java
@@ -7,23 +7,30 @@ import feign.Headers;
 import feign.RequestLine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactivefeign.spring.config.EnableReactiveFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactivefeign.spring.config.ReactiveFeignClient;
 import reactivefeign.webclient.WebReactiveFeign;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
+@EnableReactiveFeignClients
 public class DriverServiceApplication {
 
 	public static void main(String[] args) {
@@ -55,13 +62,17 @@ class Driver {
 @RestController
 class DriverController {
 	private final WebClient.Builder carClient;
+	private final CarServiceClient carServiceClient;
+	private final ReactiveCircuitBreakerFactory cbFactory;
 
-	public DriverController(WebClient.Builder carClient) {
+	public DriverController(WebClient.Builder carClient, CarServiceClient carServiceClient, ReactiveCircuitBreakerFactory cbFactory) {
 		this.carClient = carClient;
+		this.carServiceClient = carServiceClient;
+		this.cbFactory = cbFactory;
 	}
 
 	/** */
-	@GetMapping("/drivers")
+	//@GetMapping("/drivers")
 	public Mono<Driver> getDriver() {
 		/* Create instance of your API */
 		CarServiceApi client =
@@ -84,6 +95,20 @@ class DriverController {
 	}
 	/* */
 
+	@GetMapping("/drivers")
+	public Mono<Driver> getDriverUsingFeignClient() {
+		Flux<Car> carFlux = carServiceClient.getCars()
+			.transform(it-> {
+				ReactiveCircuitBreaker cb = cbFactory.create("slow");
+				return cb.run(it, throwable -> Flux.empty());
+			})
+			.filter(p -> p.getName().equals("ID. BUZZ"));
+
+		Mono<Driver> driver = Mono.just(new Driver("John"));
+
+		return driver.zipWith(carFlux.collectList()).map(r -> new Driver(r.getT1().getName(), r.getT2()));
+	}
+
 	// @GetMapping("/drivers")
 	public Flux<Car> getCars() {
 		return carClient.build().get().uri("http://localhost:8080/cars").retrieve().bodyToFlux(Car.class)
@@ -95,5 +120,11 @@ class DriverController {
 @Headers({ "Accept: application/json" })
 interface CarServiceApi {
 	@RequestLine("GET /cars")
+	Flux<Car> getCars();
+}
+
+@ReactiveFeignClient(name = "car-service")
+interface CarServiceClient {
+	@GetMapping("/cars")
 	Flux<Car> getCars();
 }
